39291d6deb418cf83c2d0810fb07638b6328b0e3
Markus <mk473203@outlook.com>
2022-09-18 20:54:05 +0300

    Changed default window size

--- src/MakeFractals.cpp
+++ src/MakeFractals.cpp
@@ -50,7 +50,7 @@ int main(int argc, char* argv[]) {
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow("Fractal Remake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    SDL_Window* window = SDL_CreateWindow("Fractal Remake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 960, window_flags);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
     SDL_GL_SetSwapInterval(1); // Enable vsync
@@ -68,7 +68,7 @@ int main(int argc, char* argv[]) {
          1.0f,  1.0f,   1.0f, 1.0f,   // top right
          1.0f, -1.0f,   1.0f, 0.0f,   // bottom right
         -1.0f, -1.0f,   0.0f, 0.0f,   // bottom left
-        -1.0f,  1.0f,   0.0f, 1.0f    // top left 
+        -1.0f,  1.0f,   0.0f, 1.0f    // top left
     };
 
     unsigned int indices[] = {
